5453554ae4592aa475e9f5c43785cee58022c3db
Makoto Honda <englishtrainee@gmail.com>
2019-01-04 17:31:04 +0900

    Add POST /todos route and refactor mongoose

---
+++ server/server_Jan4_19A.js
@@ -0,0 +1,67 @@
+var mongoose = require('mongoose');
+
+mongoose.Promise = global.Promise;
+mongoose.connect('mongodb://localhost:27017/TodoApp');
+
+var Todo = mongoose.model('Todo', {
+  text: {
+    type: String,
+    required: true,
+    minlength: 1,
+    trim: true
+  },
+  completed: {
+    type: Boolean,
+    default: false
+  },
+  completedAt: {
+    type: Number,
+    default: null
+  }
+});
+
+// var newTodo = new Todo({
+//   text: 'Cook dinner',
+// });
+//
+// newTodo.save().then((doc) => {
+//   console.log('Saved todo', doc);
+// }, (e) => {
+//   console.log('Unable to save todo');
+// });
+
+// var otherTodo = new Todo({
+//   // text: '         Edit this video          '
+//   text: 'Something to do'
+// });
+
+// var otherTodo = new Todo({
+  // text: 'Clean room',
+  // completed: true,
+  // completedAt: 123
+// });
+
+// otherTodo.save().then((doc) => {
+//   console.log(JSON.stringify(doc, undefined, 2));
+// }, (e) => {
+//   console.log('Unable to save other todo', e);
+// });
+
+var User = mongoose.model('User', {
+  email: {
+    type: String,
+    required: true,
+    trim: true,
+    minlength: 1
+  }
+});
+
+var newUser = new User({
+  email: 'abc@gmail.com        '
+});
+
+newUser.save().then((doc) => {
+  console.log(JSON.stringify(doc, undefined, 2));
+}, (e) => {
+  console.log('Unable to save new user', e);
+});
