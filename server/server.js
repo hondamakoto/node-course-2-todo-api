3338545c6b77180c9d6cb041e78b4e65bf52795a
Makoto Honda <englishtrainee@gmail.com>
2019-03-10 15:42:21 +0900

    Add test cases for GET /todos/:id

--- server/server.js
+++ server/server.js
@@ -69,6 +69,12 @@ app.get('/todos/:id', (req, res) => {
 });
 
 
+
+
+// app.get('/todos/:id', (req, res) => {
+//   res.send(req.params);
+// });
+
 app.listen(3000, () => {
   console.log('Started on port 3000');
 });
